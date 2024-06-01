d7bbac7a4c3d9390f5a80c25ffdc2e864d1820fe
Andrew Allen <63973997+Elijah-cyber7@users.noreply.github.com>
2024-05-31 22:16:53 -0500

    Update Data Structures.py
    added docstring

--- Data Structures.py
+++ Data Structures.py
@@ -2,12 +2,22 @@
 
 class Node:
     def __init__(self, data):
+        '''
+
+        :param data:
+        '''
         self.data = data
-        self.next = none
-    
+        self.next = None
+
 
 class BinaryTree:
-    def __init__(self, data):
+    def __init__(self, data, left, right):
+        '''
+
+        :param data:
+        :param left:
+        :param right:
+        '''
         self.left = left
         self.right = right
         self.data = data
